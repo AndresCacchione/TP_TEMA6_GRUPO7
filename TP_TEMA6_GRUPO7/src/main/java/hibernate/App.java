231eaf5f707095f74b0228372b803dd006f9362e
edu-salcedo <edwin_15_89@hotmail.com>
2022-05-16 20:10:53 -0300

    clases autor y nacionalidad

--- TP_TEMA6_GRUPO7/src/main/java/hibernate/App.java
+++ TP_TEMA6_GRUPO7/src/main/java/hibernate/App.java
@@ -6,23 +6,32 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
+import dao.ConfigHibernate;
+import hibernate.entidad.Autor;
+import hibernate.entidad.Nacionalidad;
+
 public class App {
 	
     public static void main( String[] args ) {
-    	 SessionFactory sessionFactory;
+    	
+    	ConfigHibernate cHibernate =new ConfigHibernate();
+    	Session session =cHibernate.abrirConexion();
+
+    	session.beginTransaction();
+
          
-         Configuration configuration = new Configuration();
-         configuration.configure();
+         Nacionalidad nacionalidad =new Nacionalidad(1,"Argentina");
+         session.save(nacionalidad);
          
-         ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
-         sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+         Autor autor=new Autor();
+         autor.setId(1);
+         autor.setNombre("juan");
+         autor.setNacionalidad(nacionalidad);
+         session.save(autor);
+         session.getTransaction().commit();
+         cHibernate.cerrarSession();;      
          
-         Session session = sessionFactory.openSession();
-         
-         session.beginTransaction();
-         
-         session.close();
-         
-         sessionFactory.close();
     }
 }
+
+
--- TP_TEMA6_GRUPO7/src/main/java/hibernate/entidad/Nacionalidad.java
+++ TP_TEMA6_GRUPO7/src/main/java/hibernate/entidad/Nacionalidad.java
@@ -2,4 +2,33 @@ package hibernate.entidad;
 
 public class Nacionalidad {
 
+	int id;
+	String descripcion;
+	
+	public Nacionalidad() {
+	
+	}
+	
+	public Nacionalidad(int id,String des) {
+		
+		this.id=id;
+		this.descripcion=des;
+	}
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 }
