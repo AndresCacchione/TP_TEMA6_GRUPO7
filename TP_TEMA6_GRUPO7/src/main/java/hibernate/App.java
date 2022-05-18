fa582f437b076cb7d95c58e8873b55fa0e012f36
aleolivera <62443592+aleolivera@users.noreply.github.com>
2022-05-18 17:06:13 -0300

    Arme el Dao de Biblioteca, la entidad Biblioteca y estableci la relacion con la clase Libro. Luego arme en Main.java un guardado de registro y un readOne para luego mostrarlo por pantalla

--- TP_TEMA6_GRUPO7/src/main/java/hibernate/App.java
+++ TP_TEMA6_GRUPO7/src/main/java/hibernate/App.java
@@ -6,9 +6,11 @@ import java.util.HashSet;
 import java.util.Set;
 import org.hibernate.Session;
 import dao.ConfigHibernate;
+import dao.DaoBiblioteca;
 import dao.DaoGenero;
 import dao.DaoLibro;
 import hibernate.entidad.Autor;
+import hibernate.entidad.Biblioteca;
 import hibernate.entidad.Genero;
 import hibernate.entidad.Libro;
 import hibernate.entidad.Nacionalidad;
@@ -54,6 +56,24 @@ public class App {
         
         Libro libro = new Libro(4432,"Jason Voorhees", new Date(2001, 12, 15), "Inglés", 350, autorPrueba,"Libro que no te deja dormir", setGeneros);
         DaoLibro.Add(libro);
+        
+        Biblioteca b1= new Biblioteca(libro, new Date(2022,05,18),1);
+        DaoBiblioteca.Add(b1);
+        
+        Biblioteca b2= DaoBiblioteca.ReadOne(1);
+        System.out.println("-----BIBLIOTECA-----");
+        System.out.println("Titulo: "+b2.getLibro().getTitulo());
+        System.out.println("Autor: "+b2.getLibro().getAutor().getNombre()+ " "+b2.getLibro().getAutor().getApellido());
+        System.out.println("Fecha de Alta: "+b2.getFechaAlta());
+        if(b2.getEstado()==1) {
+        	System.out.println("Estado: En biblioteca");
+        }
+        else if(b2.getEstado()==2) {
+        	System.out.println("Estado: Prestado");
+        }
+        else {
+        	System.out.println("Estado: Sin definir");
+        }
     }
 }
 
