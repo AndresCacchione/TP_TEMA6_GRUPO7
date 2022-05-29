ed511305eaa9e807e0249644d0d45cbcbc95dc56
Andres Cacchione <andres.cacchione@gmail.com>
2022-05-29 00:18:41 -0300

    Creación: - Método ReadClassOrderAscByISBN en DaoLibro - toStringLibroPropp en Libro - AppHQL main

--- TP_TEMA6_GRUPO7/src/main/java/dao/DaoLibro.java
+++ TP_TEMA6_GRUPO7/src/main/java/dao/DaoLibro.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.sql.Date;
+import java.util.List;
 
 import org.hibernate.Session;
 
@@ -9,6 +10,19 @@ import hibernate.entidad.Libro;
 public class DaoLibro {
 	private static ConfigHibernate cHibernate;
 	private static Session session;
+	
+	public static void ReadClassOrderAscByISBN() {
+		cHibernate = new ConfigHibernate();
+		session = cHibernate.abrirConexion();
+		List<Libro> listaLibros = (List<Libro>) session.createQuery("FROM Libro lib ORDER BY lib.isbn DESC").list();
+		
+		System.err.println("Libros ordenados por ISBN (de mayor a menor): ");
+		for (Libro libro : listaLibros) {
+			System.out.println("Libro: " + libro.toStringLibroPropp());
+		}
+		
+		cHibernate.cerrarSession();
+	}
 	
 	public static void Add(Libro libro) {
 		cHibernate = new ConfigHibernate();
